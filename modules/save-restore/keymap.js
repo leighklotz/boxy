e500223fc6345873137909d797f3918582bb740f
Leigh Klotz <klotz@klotz.me>
2025-02-06 17:55:04 -0800

    add menus for save/restore

--- modules/save-restore/keymap.js
+++ modules/save-restore/keymap.js
@@ -1,3 +1,7 @@
 keyMap['Ctrl-o'] = loadBoxFromString
 keyMap['Ctrl-%'] = downloadSerializedCurrentBox
 keyMap['Ctrl-^'] = downloadSerializedBoxy
+
+addToMenu('Load Box (specify filename)', loadBoxFromString, 'Ctrl-o');
+addToMenu('Download whole Boxy', downloadSerializedBoxy, 'Ctrl-^');
+addToMenu('Download current box', downloadSerializedCurrentBox, 'Ctrl-%');
