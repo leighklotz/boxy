d5ed671ef5fee66c568afefa81e66a9c605718cb
Leigh Klotz <klotz@klotz.me>
2025-02-11 13:58:06 -0800

    run.sh: use env vars for port and bind

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+BIND_IP="127.0.0.1"
+#BIND_IP="0.0.0.0"
+PORT=8080
+
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-python3 -m http.server 8080 --bind 127.0.0.1 --directory "${SCRIPT_DIR}"
+python3 -m http.server "${PORT}" --bind "${BIND_IP}" --directory "${SCRIPT_DIR}"
 
 
